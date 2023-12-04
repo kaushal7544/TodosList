edcc2105c5a8018b5b5d85c238053b4d53576265
kaushalvaghela <kaushalvaghela2004@gmail.com>
2023-12-04 16:54:32 +0530

    new project added

---
+++ src/MyComponents/Header.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Header(props) {
+  return (
+    <div>
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">{props.title}</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+       
+      </ul>
+      { props.searchbar? <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form> : ""}
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+Header.defaultProps = {
+  title: "Your Title Here",
+  searchbar:true
+}
+
+ Header.propTypes = {
+  title: PropTypes.string ,
+  searchbar: PropTypes.bool.isRequired
+ }
+
+export default Header
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+const Todos = (props) => {
+  return (
+    <div className="container">
+      <h3>Todos List</h3>
+      <TodoItem todo={props.todos[0]}/>
+    </div>
+  )
+}
+
+export default Todos
